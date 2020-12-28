c453be53bca616ea166849fb1975ce742a4029a8
nekomiao123 <447140443@qq.com>
2020-12-28 19:00:31 +0800

    3.0 comes

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 **本打卡脚本仅供学习交流使用，请勿过分依赖。开发者对使用或不使用本脚本造成的问题不负任何责任，不对脚本执行效果做出任何担保，原则上不提供任何形式的技术支持。**
 
-## 1.0版本简介 （healsign.py就是1.0版本）
+## 1.0版本简介 （healthsign.py就是1.0版本）
 
 此脚本使用的是python + selenium库的方式进行模拟人工打卡，并使用了chrome driver去控制chrome浏览器
 
@@ -21,7 +21,7 @@ pip install selenium
 - 值得注意的是chromedriver的版本号要和chrome一致才行（在chrome浏览器设置里面的About Chrome就可以看到chrome版本号了）
 - 打开healthsign.py文件，在usernames的列表里面以字符串的形式填入学号运行即可
 
-## 2.0版本会直接抓包执行（healsign2.py就是2.0版本）
+## 2.0版本会直接抓包执行（healthsign2.py就是2.0版本）
 
 ## 2.0来了 
 
@@ -45,6 +45,30 @@ pip install requests
 去google浏览器里面按F12进入开发者模式 然后按下图的图标 变成手机模式 打卡一次 以后就好了（我也不知道为什么）
 
 ![image-20201226205958468](https://cdn.jsdelivr.net/gh/nekomiao123/pic/img/image-20201226205958468.png)
+
+## 3.0 来了（healthsign3.py就是3.0）
+
+我写完2.0，用了第一天就发现2.0有问题，只有在当天打过卡之后，我才能得到历史数据（总的来说，就是我写了个寂寞）
+
+3.0就这么出世了
+
+### 食用说明
+
+- 需要先安装requests
+
+```
+pip install requests
+```
+
+- 开发环境是python 3.8.2 
+- 这一个版本直接跑就可以了
+- 文字会提示是否已经打过卡（目前测试样例很少，但应该是准确的）
+
+### 图示
+
+![](https://cdn.jsdelivr.net/gh/nekomiao123/pic/img/image-20201228185614331.png)
+
+
 
 # 未完成情况
 
--- healthsign3.py
+++ healthsign3.py
@@ -4,6 +4,7 @@ import urllib3
 import json
 import gzip
 import os
+import datetime
 
 ''' 
     每日打卡3.0版
@@ -24,6 +25,299 @@ import os
 '''
 
 
+class HealthSign(object):
+    def __init__(self, username, password):
+        super().__init__()
+        self.username = username
+        self.password = password
+        # 每个URL后面都应该加上当时的时间戳
+        self.loginUrl = "https://xsc-health.wh.sdu.edu.cn/mobile/rpc?p=/v2/login/login&t="
+        self.idUrl = "https://xsc-health.wh.sdu.edu.cn/mobile/rpc?p=/v2/fight/ncp/health/report/getId&t="
+        self.oldTimeUrl = "https://xsc-health.wh.sdu.edu.cn/mobile/rpc?p=/v2/cmdb/ci/getById&t="
+        self.getDataUrl = "https://xsc-health.wh.sdu.edu.cn/mobile/rpc?p=/v2/workorder/formRule/execute&t="
+        self.signUrl = "https://xsc-health.wh.sdu.edu.cn/mobile/rpc?p=/v2/workorder/action/createWithValidate&t="
+
+        self.signSession = requests.Session()
+        self.signHeader = {
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36',
+            "content-type": "application/json"
+            }
+        self.loginCookie = None
+        self.loginId = None
+        self.userBasicInfo = None
+        self.userAllInfo = None
+        self.length = 0
+
+    # 创建13位时间戳
+    def getTimeStamp(self):
+        now = time.time()
+        now = (int(now * 1000))
+        strnow = str(now)
+        return strnow
+
+    # 登录函数
+    def healthLogin(self):
+        loginTime = self.getTimeStamp()
+        realLoginUrl = self.loginUrl + loginTime
+        loginData = {
+            "jsonrpc": "2.0",
+            "method": "/v2/login/login",
+            "id": 1,
+            "params": [self.username, self.password, "false"]
+        }
+        try:
+            r = self.signSession.post(realLoginUrl, json=loginData, verify=False)
+            str1 = str(r.cookies)
+            self.loginCookie = str1[27:67]
+            print("登录成功")
+        except:
+            print("登录失败，请检查账号密码是否正确")
+        
+        return self.signSession
+
+    # 获取id
+    def getId(self):
+        idTime = self.getTimeStamp()
+        realIdUrl = self.idUrl + idTime
+        idData = {
+            "jsonrpc": "2.0",
+            "method": "/v2/fight/ncp/health/report/getId",
+            "id": 1,
+            "params": []
+        }
+        try:
+            idRequest = self.signSession.post(realIdUrl, json=idData, headers = self.signHeader)
+            idRJson = idRequest.json()
+            self.loginId = idRJson['result']
+            print("获取id成功")
+            # print("realID is ")
+            # print(self.loginId)
+        except:
+            print("id获取失败")
+    
+    # 获取基础用户信息
+    def getOldUserInfo(self):
+        oldTimeT = self.getTimeStamp()
+        realOldTimeUrl = self.oldTimeUrl + oldTimeT
+        # params的第一个参数是最后的type，并不知道是什么，但是测试了好几次，这个type是不变的
+        oldTimeData = {
+            "jsonrpc": "2.0",
+            "method": "/v2/cmdb/ci/getById",
+            "id": 1,
+            "params": ["40bca208-5184-11ea-887d-cb65bdaac481", self.loginId]
+        }
+        try:
+            oldTimeRequests = self.signSession.post(realOldTimeUrl, json=oldTimeData, headers=self.signHeader)
+            # print("oldTImeRequest")
+            # print(oldTimeRequests.headers)
+            oldHeaderJson = oldTimeRequests.headers
+            # 虽然很无奈 但是好像只能用这个length来判断打过卡没
+            self.length = oldHeaderJson['Content-Length']
+            self.length = int(self.length)
+            # print("Content-Length is")
+            print(self.length)
+            oldTRJson = oldTimeRequests.json()
+
+            # 针对返回情况的优化
+            shifouquedingfanhuishijian = ""
+            fanhuishijian = ""
+            if "shifouquedingfanhuishijian" in oldTRJson['result']:
+                shifouquedingfanhuishijian = oldTRJson['result']['shifouquedingfanhuishijian']['name']
+            if "fanhuishijian" in oldTRJson['result']:
+                fanhuishijian = oldTRJson['result']['fanhuishijian']
+            
+            self.userBasicInfo = {
+                "apply_user": oldTRJson['result']['apply_user']['id'],
+                "xm": oldTRJson['result']['xm'],
+                "xh": oldTRJson['result']['xh'],
+                "xb": oldTRJson['result']['xb']['name'],
+                "lxdh": oldTRJson['result']['lxdh'],
+                "fanhuishijian": fanhuishijian,
+                "id": self.loginId,
+                "type": "40bca208-5184-11ea-887d-cb65bdaac481",
+                "source": "mobile"
+            }
+            #这里可以输出用户名字
+            # print(self.userBasicInfo['xm'])
+            
+            if self.length > 1000:
+                self.userAllInfo = {
+                    "xllb": oldTRJson['result']['xllb']['name'],              
+                    "szyx": oldTRJson['result']['szyx']['id'],    
+                    "tiwen": oldTRJson['result']['tiwen'],   
+                    "dw": oldTRJson['result']['dw'],
+                    "shifouzaixiao": oldTRJson['result']['shifouzaixiao']['name'],                                  
+                    "shifouyifanhuihuocongweilikaixuexiao": oldTRJson['result']['shifouyifanhuihuocongweilikaixuexiao']['name'],      
+                    "muqiansuozaichengshi": oldTRJson['result']['muqiansuozaichengshi']['name'],                
+                    "sheng": oldTRJson['result']['sheng']['id'],         
+                    "shi": oldTRJson['result']['shi']['id'],           
+                    "qu": oldTRJson['result']['qu']['id'],            
+                    "xxdz": oldTRJson['result']['xxdz'],     
+                    "shifouquedingfanhuishijian": shifouquedingfanhuishijian,                     
+                    "fanhuishijian": fanhuishijian,    
+                }
+                print("该用户已经打过卡，获取数据成功")
+
+            # print("用户基础数据")
+            # print(self.userBasicInfo)
+        except:
+            print("获取用户基础数据失败")
+
+
+    # 获取用户所有信息
+    def getAllInfo(self):
+        allTime = self.getTimeStamp()
+        realAllUrl = self.getDataUrl + allTime
+        allData = {
+            "jsonrpc": "2.0",
+            "method": "/v2/workorder/formRule/execute",
+            "id": 1,
+            "params": ["8a525ad7-5187-11ea-a13f-53bf2079bf35", {
+                "type": "init",
+                "param": True
+            }, {
+                "data": {
+                    "apply_user": self.userBasicInfo["apply_user"],
+                    "xm": self.userBasicInfo['xm'],
+                    "xh": self.userBasicInfo['xh'],
+                    "xb": self.userBasicInfo['xb'],
+                    "lxdh": self.userBasicInfo['lxdh'],
+                    "fanhuishijian": self.userBasicInfo['fanhuishijian'],
+                    "id": self.userBasicInfo['id'],
+                    "type": self.userBasicInfo['type'],
+                    "source": self.userBasicInfo['source']
+                },
+                "params": {}
+            }]
+        }
+
+        try:
+            getAllRequests = self.signSession.post(realAllUrl, json=allData, headers=self.signHeader)
+            getAllJson = getAllRequests.json()
+            # print(getAllRequests.headers)
+            # 输出这个文件能看到详细的用户数据
+            # fJson = json.dumps(getAllJson, sort_keys=True, indent=4, ensure_ascii=False)
+            # with open('Allinfo.json', 'w') as f:
+            #     f.write(fJson)
+            # 只有length<1000的时候logs里面才有正确的值
+            if self.length < 1000:
+                logs = getAllJson["result"]["logs"]
+                self.userAllInfo = logs['日志格式错误，请修改脚本']['page']['data'][0]
+                print("该用户尚未打卡，获取用户所有数据成功")
+                # print(type(self.userAllInfo))
+                # print(self.userAllInfo)
+
+        except:
+            print("无法获取用户全部数据")
+
+    # 打卡函数
+    def signAll(self):
+        signTime = self.getTimeStamp()
+        realSignUrl = self.signUrl + signTime
+
+        # 针对返回情况的优化
+        shifouquedingfanhuishijian = ""
+        fanhuishijian = ""
+        if "shifouquedingfanhuishijian" in self.userAllInfo:
+            shifouquedingfanhuishijian = self.userAllInfo['shifouquedingfanhuishijian']
+        if "fanhuishijian" in self.userAllInfo:
+            fanhuishijian = self.userAllInfo['fanhuishijian']
+
+        # 这个data特别多 
+        signData = {
+        "jsonrpc": "2.0",
+        "method": "/v2/workorder/action/createWithValidate",
+        "id": 1,
+        "params": [
+            [
+                {
+                    "id": self.loginId, 
+                    "type": self.userBasicInfo['type'],
+                    "source": self.userBasicInfo['source'], 
+                    "apply_user": self.userBasicInfo['apply_user'],    
+                    "xllb": self.userAllInfo['xllb'],              
+                    "szyx": self.userAllInfo['szyx'],          
+                    "xm": self.userBasicInfo['xm'],
+                    "xh": self.userBasicInfo['xh'],
+                    "xb": self.userBasicInfo['xb'],    
+                    "lxdh": self.userBasicInfo['lxdh'],             
+                    "jkzt": "健康",                             
+                    "shifoufare": "否",                                       
+                    "tiwen": self.userAllInfo['tiwen'],                                           
+                    "shifoujiuzhenzhuyuan": "",                              
+                    "yiyuanmingcheng": "",                                   
+                    "shifougeli": "fou",                                     
+                    "gelifangshi": "",                                       
+                    "gelidizhi": "",                                         
+                    "dw": self.userAllInfo['dw'],       
+                    "cunjieqijianshifouzaixiao": "fou",                      
+                    "shifouzaixiao": self.userAllInfo['shifouzaixiao'],                                  
+                    "shifouyifanhuihuocongweilikaixuexiao": self.userAllInfo['shifouyifanhuihuocongweilikaixuexiao'],      
+                    "muqiansuozaichengshi": self.userAllInfo['muqiansuozaichengshi'],                
+                    "sheng": self.userAllInfo['sheng'],         
+                    "shi": self.userAllInfo['shi'],           
+                    "qu": self.userAllInfo['qu'],            
+                    "xxdz": self.userAllInfo['xxdz'],                                       
+                    "guowaidizhi": "",                                       
+                    "shifouquedingfanhuishijian": shifouquedingfanhuishijian,                     
+                    "fanhuishijian": fanhuishijian,                          
+                    "jinyigeyueshifouquguohubei": "fou",                     
+                    "jinyigeyueshifoujiechuguoquezhenbingli": "fou",         
+                    "jinyigeyueshifoujiechuguoyisibingli": "fou",            
+                    "miqiejiechuguanxi": "",                                 
+                    "ganranzhe": "否",                                       
+                    "jiechuzhe": "否",                                       
+                    "juzhu": "否",                                           
+                    "fare": "否",                                            
+                    "hubeijingwai": "否"    
+                    }
+                    ],
+                    [
+                        "8a525ad7-5187-11ea-a13f-53bf2079bf35"
+                    ]
+                ]
+            }
+        # 这里能输出最后打卡的数据
+        # json_str = json.dumps(signData, sort_keys=True, indent=4, ensure_ascii=False)
+        # # filename = 'duibi_info' + str(t) + '.json'
+        # with open("finalSend.json", 'w') as f:
+        #     f.write(json_str)
+        # print("输出待提交的打卡数据成功")
+        try:
+            signRequest = self.signSession.post(realSignUrl, json=signData, headers=self.signHeader)
+            # print("signRequest: ")
+            # print(signRequest.text)
+
+            end = "<Response [200]>"
+            if end == str(signRequest):
+                print("打卡成功")
+            else:
+                print("打卡失败")
+        except:
+            print("提交打卡数据失败")
 
 
 
+def main(username, password):
+    print("\n[Time] %s" %datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    print("🚌 打卡任务启动")
+    daka = HealthSign(username, password)
+
+    daka.healthLogin()
+    
+    daka.getId()
+
+    daka.getOldUserInfo()
+
+    daka.getAllInfo()
+
+    daka.signAll()
+    print("打卡任务结束")
+
+
+if __name__ == "__main__":
+    # 忽略TLS Warnings警告
+    urllib3.disable_warnings()
+    username = "填入学号"
+    password = "whsdu@" + username
+    main(username, password)
